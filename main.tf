47cd90efe32b8c0fb983d70c02c76d7152ac0bb4
SanjaiSaran <106731624+SanjaiSaran@users.noreply.github.com>
2022-07-21 12:01:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,14 +13,12 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "good_example" {
+resource "aws_instance" "bad_example" {
   ami = "ami-7f89a64f"
   instance_type = "t1.micro"
- 
- }
 
   root_block_device {
-      encrypted = true
+      encrypted = false
   }
 
   ebs_block_device {
@@ -28,6 +26,7 @@ resource "aws_instance" "good_example" {
     volume_size = 5
     volume_type = "gp2"
     delete_on_termination = false
-    encrypted = true
+    encrypted = false
   }
 }
+
