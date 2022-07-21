877655a63b3c5d7306666a6b47789d7617ec785d
SanjaiSaran <106731624+SanjaiSaran@users.noreply.github.com>
2022-07-21 11:36:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "bad_example" {
+resource "aws_instance" "good_example" {
   ami = "ami-7f89a64f"
   instance_type = "t1.micro"
 
   root_block_device {
-      encrypted = false
+      encrypted = true
   }
 
   ebs_block_device {
@@ -26,6 +11,6 @@ resource "aws_instance" "bad_example" {
     volume_size = 5
     volume_type = "gp2"
     delete_on_termination = false
-    encrypted = false
+    encrypted = true
   }
 }
