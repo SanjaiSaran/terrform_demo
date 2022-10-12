536834e20e248adc487e5e5259a791f0bd71b109
Sanjai Saran M <106731624+SanjaiSaran@users.noreply.github.com>
2022-10-12 13:34:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+ resource "aws_ebs_volume" "bad_example" {
+   availability_zone = "us-west-2a"
+   size              = 40
 
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
+   tags = {
+     Name = "HelloWorld"
+   }
+   encrypted = false
+ }
